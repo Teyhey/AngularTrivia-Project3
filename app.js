7654da7d23975e38f524bcc6f190665ee94fe7bb
Sean Whiteman <seanw265@gmail.com>
2017-11-01 16:25:17 -0400

    playing around

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const app = express();
 
 const apiRouter = express.Router();
 
+var mongoose = require('mongoose');
+ 
+mongoose.connect('mongodb://express:EXPRESS@ds243325.mlab.com:43325/cisc474');
+
 app.use("/api", apiRouter);
 
 apiRouter.get("/", function (req, res) {
   res.send("api");
+});
+
+apiRouter.post("/user", function (req, res) {
+  res.send("users");
 });
 
 app.get('/', function (req, res) {
