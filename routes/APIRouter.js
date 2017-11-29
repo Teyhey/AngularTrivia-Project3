36dc780a5bbbd976bd591e14a8acabcc62d4dad2
Sean Whiteman <seanw265@gmail.com>
2017-11-29 16:44:42 -0500

    Submit scores and get top scores

--- routes/APIRouter.js
+++ routes/APIRouter.js
@@ -14,13 +14,43 @@ apiRouter.get("/test", function (req, res) {
   s.score = 12;
   s.username = "testUser"
   s.date = Date.now();
-  s.save();
+  s.save(function(err){
+  	if(err){
+  		res.sendStatus(500);
+  		res.send("Something went wrong.");
+  		return;
+  	}else{
+  		res.sendStatus(200);
+  		res.send("Test object sent to scores collection.");
+  		return;
+  	}
+  });
 
-  res.send("test object sent to scores collection");
+  res.sendStatus(500);
+  res.send("Something went wrong.");
+  return;
 });
 
-apiRouter.post("/user", function (req, res) {
-  res.send("users");
+apiRouter.get("/topScores", function (req, res) {
+  Score.find().sort({score:-1}).limit(10).exec(function (err, result) {
+    res.status(200).json({result : result});
+  })
+});
+
+apiRouter.post("/submitScore", function (req, res) {
+  var s = new Score();
+  s.score = req.body.score;
+  s.username = req.body.username;
+  s.date = Date.now();
+  s.category = req.body.category;
+  s.difficulty = req.body.difficulty;
+  s.save(function(err){
+    if(err){
+      res.sendStatus(500);
+    }else{
+      res.sendStatus(200);
+    }
+  });
 });
 
 module.exports = apiRouter;
