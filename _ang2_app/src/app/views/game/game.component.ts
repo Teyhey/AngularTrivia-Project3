6ab531ccd5f361654337b98162717315d5988bca
dusty22 <creinhardt228@yahoo.com>
2017-12-13 14:52:45 -0500

    bootstrap and jquery for game page

--- _ang2_app/src/app/views/game/game.component.ts
+++ _ang2_app/src/app/views/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-game',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+	$(".center-logo").hide();
+	$(".jumbotron").hide();
+	$("html").css("background-image", )
+  }
 
   ngOnInit() {
   }
