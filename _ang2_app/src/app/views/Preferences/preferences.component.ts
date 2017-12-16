e14f1b319f4a0ad2902ab9c9d773e215440a7c32
Hayley Andrews <hayzaffre@gmail.com>
2017-12-15 20:18:44 -0500

    Showing friends

--- _ang2_app/src/app/views/Preferences/preferences.component.ts
+++ _ang2_app/src/app/views/Preferences/preferences.component.ts
@@ -20,10 +20,80 @@ export class PreferencesComponent implements OnInit {
   friends;
   requestsent;
   requestpending;
+  friendarray;
+  friendview = [];
+  friendlength = 0;
+  currentpage = 1;
+  friendcount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   ngOnInit() {
 	$(".center-logo").show();
-	$(".jumbotron").show();
+  $(".jumbotron").show();
+  
+  this.triviaservice.getFriends().subscribe(
+    data => {
+      this.friendarray = data['result'];
+      let i = 0;
+      this.friendlength = data['result'].length;
+      while ((i < 10) && (i < this.friendlength)) {
+        this.friendview[i] = data['result'][i];
+        i = i + 1;
+      }
+    },
+    error => alert(error),
+    () => console.log('Finished')
+  );
+  }
+
+  nextTenFriends() {
+    if (this.checklarger()) {
+      this.currentpage = this.currentpage + 10;
+      let i = this.currentpage - 1;
+      let j = 0;
+      let k = 0;
+      while ((i < (this.currentpage + 9)) && (i < this.friendlength)) {
+        this.friendview[j] = this.friendarray[i];
+        i++;
+        j++;
+      }
+      while (k < (this.currentpage + 9)) {
+        this.friendcount[k] = this.friendcount[k] + 10;
+        k++;
+      }
+    } else {
+      console.log('Not enough friends');
+    }
+  }
+
+  previousTenFriends() {
+    if (this.checksmaller()) {
+      this.currentpage = this.currentpage - 10;
+      let i = this.currentpage - 1;
+      let j = 0;
+      while ((i < (this.currentpage + 9)) && (i < this.friendlength)) {
+        this.friendview[j] = this.friendarray[i];
+        i++;
+        j++;
+      }
+    } else {
+      console.log('Can not go further back');
+    }
+  }
+
+  checklarger() {
+    if ((this.currentpage + 10) > this.friendlength) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  checksmaller() {
+    if ((this.currentpage - 10) < 0) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   logoutClick() {
